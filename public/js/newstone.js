74047fa983c2d25d28ca6dae0080030255abed93
spaghetti <carbonetti.marco@gmail.com>
2024-10-24 23:02:41 +0200

    piccoli aggiustamenti

--- public/js/newstone.js
+++ public/js/newstone.js
@@ -5,15 +5,21 @@ const color = document.querySelectorAll('.color-btn')
 
 
 const disableAllBtn = ()=>{
-    color.forEach(btn => btn.style.border = 'none')
+    color.forEach((btn,ind) => {
+        let btnSelection = document.getElementById(ind)
+        btn.style.border = 'none'
+        btnSelection.style.boxShadow = '0px 0px 10px 2px lightgray'
+    })
 }
 
 color.forEach((btn,ind)=>{
+    btn.style.boxShadow = '0px 0px 10px 2px lightgray'
     btn.id = ind
     let btnSelection = document.getElementById(ind)
     btn.addEventListener('click',()=>{
         disableAllBtn()
-        btnSelection.style.border = '0.5px solid #FF2D55'
+        btnSelection.style.border = `1px solid ${btnSelection.style.backgroundColor}`
+        btnSelection.style.boxShadow = `0 0 25px 6px ${btnSelection.style.backgroundColor}`
         
     })
 
